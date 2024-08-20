2628c2ef270da915cfb09dd8c28dd04974e0918c
ccbc7 <flexibleelbow@gmail.com>
2024-08-20 20:27:46 +0900

    多分docker立ち上がった

--- backend/main.go
+++ backend/main.go
@@ -27,5 +27,5 @@ func main() {
 	r.POST("/items", itemController.Create)
 	r.PUT("/items/:id", itemController.Update)
 	r.DELETE("/items/:id", itemController.Delete)
-	r.Run("localhost:8080")
+	r.Run(":8080")
 }
