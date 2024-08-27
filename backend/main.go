7b56c763231ae2f9e60de3b642495ba5d0eccfd6
ccbc7 <flexibleelbow@gmail.com>
2024-08-27 17:44:56 +0900

    ひとまずログイン成功

--- backend/main.go
+++ backend/main.go
@@ -42,5 +42,6 @@ func main() {
 	itemRouter.DELETE("/:id", itemController.Delete)
 
 	authRouter.POST("/signup", authController.SignUp)
+	authRouter.POST("/login", authController.Login)
 	r.Run(":8080")
 }
