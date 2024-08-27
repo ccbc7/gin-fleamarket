0d2763a7ea776bd152637cba3d7879f594a19879
ccbc7 <flexibleelbow@gmail.com>
2024-08-27 18:55:53 +0900

    リレーションの実装

--- backend/models/user.go
+++ backend/models/user.go
@@ -6,4 +6,6 @@ type User struct {
 	gorm.Model
 	Email    string `gorm:"not null;unique"`
 	Password string `gorm:"not null"`
+	// ユーザーが削除された場合、そのユーザーのアイテムも削除される
+	Items []Item `gorm:"constraint:OnDelete:CASCADE"`
 }
