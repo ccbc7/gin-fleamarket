2ab547502b6f538da91392e76be9b667a3e829a4
ccbc7 <flexibleelbow@gmail.com>
2024-08-27 18:17:40 +0900

    トークンを取得できるようになった

--- backend/services/auth_service.go
+++ backend/services/auth_service.go
@@ -1,8 +1,13 @@
 package services
 
 import (
-	"gin-fleamarket/repositories"
+	"os"
+	"time"
+
 	"gin-fleamarket/models"
+	"gin-fleamarket/repositories"
+
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -39,10 +44,35 @@ func (s *AuthService) Login(email string, password string) (*string, error) {
 		return nil, err
 	}
 
+	// パスワードの比較
 	err = bcrypt.CompareHashAndPassword([]byte(foundUser.Password), []byte(password))
 	if err != nil {
 		return nil, err
 	}
 
-	return &foundUser.Email, nil
+	// トークンの作成
+	token, err := CreateToken(foundUser.ID, foundUser.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func CreateToken(userId uint, email string) (*string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		// subはsubjectの略で、JWTの主題を表す
+		// emailはユーザーのメールアドレス
+		// expはJWTの有効期限を表す
+		"sub":   userId,
+		"email": email,
+		"exp":   time.Now().Add(time.Hour).Unix(),
+	})
+
+	// 署名
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+	if err != nil {
+		return nil, err
+	}
+	return &tokenString, nil
 }
